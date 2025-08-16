eeabb409992c770f59e0400263f7562ce29cf196
Himanshu Kashyap <hkashyap.developer@gmail.com>
2025-08-16 13:01:57 +0530

    Mobile hero font size updated.

--- src/components/custom/herobanner/herobanner.tsx
+++ src/components/custom/herobanner/herobanner.tsx
@@ -6,7 +6,7 @@ export default function HeroSectionOne() {
   return (
     <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
       <div className="px-4 py-10 md:py-20">
-        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
+        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-4xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
           {"Launch your website in hours, not days"
             .split(" ")
             .map((word, index) => (
